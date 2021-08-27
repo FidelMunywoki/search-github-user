563c8a4ab2f64ad2847c3b5fe45f5a87d4d02018
FidelMunywoki <fidelis.munywoki@student.moringaschool.com>
2021-08-27 21:59:37 +0300

    add model url

--- src/app/model/user-url.ts
+++ src/app/model/user-url.ts
@@ -1,2 +1,17 @@
 export class UserUrl {
+
+    constructor (
+        public url : string,
+        public login : any,
+        public name : any,
+        public html_url : any,
+        public public_repos : any,
+        public following : any,
+        public followers : any,
+        public created_at : any,
+        public location : any,
+        public avatar_url : any,
+        public bio : any,
+
+    ) {}
 }
