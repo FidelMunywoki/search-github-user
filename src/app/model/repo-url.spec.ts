24525500bc62a3017d114ac56ee6f653b2464cbf
FidelMunywoki <fidelis.munywoki@student.moringaschool.com>
2021-08-27 21:42:04 +0300

    add models

---
+++ src/app/model/repo-url.spec.ts
@@ -0,0 +1,7 @@
+import { RepoUrl } from './repo-url';
+
+describe('RepoUrl', () => {
+  it('should create an instance', () => {
+    expect(new RepoUrl()).toBeTruthy();
+  });
+});
---
+++ src/app/model/user-url.spec.ts
@@ -0,0 +1,7 @@
+import { UserUrl } from './user-url';
+
+describe('UserUrl', () => {
+  it('should create an instance', () => {
+    expect(new UserUrl()).toBeTruthy();
+  });
+});
