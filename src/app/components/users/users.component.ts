7a56c0d3c21e438ae316bf18ea36e61fff20f050
FidelMunywoki <fidelis.munywoki@student.moringaschool.com>
2021-08-28 12:09:07 +0300

    update

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input,OnInit } from '@angular/core';
+import { RepoUrl } from 'src/app/model/repo-url';
+import { UserUrl } from 'src/app/model/user-url';
+import { SearchService } from 'src/app/services/search.service';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +10,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  repos : RepoUrl;
+  users : UserUrl[];
+
+
+
+  constructor(private search: SearchService) { }
 
   ngOnInit(): void {
+
+    this.searchUsers('FidelMunywoki');
+    this.searchRepos('FidelMunywoki');
+  }
+
+  //search user
+
+  searchUsers(searchTerm: string) {
+    this.search.searchUser(searchTerm).then(
+      () => {
+        this.users = this.search.users;
+      },
+      (error) => {
+        console.log(error)
+      }
+    )
+  }
+
+
+  //search Repos
+  searchRepos(searchTerm: string) {
+    this.search.searchRepo(searchTerm).then(
+      () => {
+        this.repos = this.search.repos;
+      },
+      (error) => {
+        console.log(error)
+      }
+    )
   }
 
 }
