73f3c5b07ae9a3d1dfdddc25153ec7d6dd64da4c
FidelMunywoki <fidelis.munywoki@student.moringaschool.com>
2021-08-28 12:09:32 +0300

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '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',
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
