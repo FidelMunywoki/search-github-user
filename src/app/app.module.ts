8bf7dba0d7bf2571a676104dba367b674ffb7ba8
FidelMunywoki <fidelis.munywoki@student.moringaschool.com>
2021-08-30 13:04:53 +0300

    add Not Found

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { UsersComponent } from './components/users/users.component';
 import { SearchFormComponent } from './components/search-form/search-form.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
-    SearchFormComponent
+    SearchFormComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
